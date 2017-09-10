67590ca2514461dd79cb3b961be90ede9624e4bb
garethgd <garethgdunne@gmail.com>
2017-09-10 17:53:00 +0100

    first commit

---
+++ src/components/hello/Hello.tsx
@@ -0,0 +1,41 @@
+// src/components/Hello.tsx
+
+import * as React from 'react';
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
+}
+
+class Hello extends React.Component<any, any> {
+  render() {
+    const { name, enthusiasmLevel = 1, onIncrement, onDecrement } = this.props;
+
+    if (enthusiasmLevel <= 0) {
+      throw new Error('You could be a little more enthusiastic. :D');
+    }
+
+    return (
+       <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Hello;
+
+// helpers
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
+
---
+++ src/containers/Hello.tsx
@@ -0,0 +1,23 @@
+// src/containers/Hello.tsx
+
+import Hello from '../components/hello/Hello';
+import * as actions from '../actions/';
+import { StoreState } from '../types/index';
+import { connect, Dispatch } from 'react-redux';
+
+export function mapStateToProps({ enthusiasmLevel, languageName }: StoreState) {
+  return {
+    enthusiasmLevel,
+    name: languageName,
+  }
+}
+
+export function mapDispatchToProps(dispatch: Dispatch<actions.EnthusiasmAction>) {
+  return {
+    onIncrement: () => dispatch(actions.incrementEnthusiasm()),
+    onDecrement: () => dispatch(actions.decrementEnthusiasm()),
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hello);
